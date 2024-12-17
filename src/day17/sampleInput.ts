43816288656114c06de018c51a236063b1c6feb1
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-12-17 17:15:26 +0100

    day17

---
+++ src/day17/sampleInput.ts
@@ -0,0 +1,16 @@
+export const rawSampleInput = {
+  part1: `Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0`,
+  part2: `Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0`,
+};
+export const sampleSolutions = {
+  part1: "4,6,3,5,6,3,5,2,1,0",
+  part2: 117440,
+} as const;
